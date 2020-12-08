f945a733666f2ebb27b217549d3636c721cc6a80
Will Dane <enadlliw@gmail.com>
2020-12-07 21:01:33 -0500

    added tables

---
+++ tables.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS employee_tracker_db;
+
+CREATE DATABASE employee_tracker_db;
+
+USE employee_tracker_db;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30)  NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30)  NULL,
+    salary DECIMAL(10, 2)  NULL,
+    department_id INT  NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30)  NULL,
+    last_name VARCHAR(30)  NULL,
+    role_id INT NULL,
+    manager_id INT NULL
+);
