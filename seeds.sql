d4a4f80829a32115bd7554730cc78ae9f0115475
Will Dane <enadlliw@gmail.com>
2020-12-07 21:30:09 -0500

    added seeds

---
+++ seeds.sql
@@ -0,0 +1,20 @@
+INSERT INTO department (name)
+VALUES  ('Legal'), ('Sales'), ('Finance'), ('Engineering');
+
+INSERT INTO role (title, salary, department_id)
+VALUES ('Manager', 140000.00, 1),
+ ('Sales Lead', 76000.00, 2),
+ ('Salesperson', 66000.00, 3),
+ ('Engineer', 85000.00, 4),
+ ('Accountant', 92000.00, 5),
+ ('Legal Team Lead', 138000,00, 6);
+ ('Lawyer', 125000.00, 7),
+
+ INSERT INTO employee (first_name, last_name, role_id, manager_id)
+ VALUES ('Will', 'Dane', 1),
+ ('Jon', 'Smith', 2, 1),
+ ('Sarah', 'Marshall', 3, 2),
+ ('Carmen', 'San Diego', 4),
+ ('Lara', 'Croft', 5, 1),
+ ('John', 'Wick', 6),
+ ('Alfred', 'Hitchcock', 7, 6);
